423a85dc1f0e19b69f1d1a1a184621e662c41138
Aishwarya S R <sraishusr@gmail.com>
2021-03-28 15:56:27 +0530

    added teachers_scrapped_data in views.py

--- practice/profile_builder/views.py
+++ practice/profile_builder/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import auth
-from .models import Students,State,Teachers
+import csv
+from . models import Students,State,Teachers,Teachers_scrapped_data
+import pandas as pd
 # Create your views here.
 
 def index(request):
+    if(Teachers_scrapped_data.objects.count()==0):
+        df = pd.read_csv('./scrapped_data_all.csv')
+        nor = df.shape[0]
+        noc = df.shape[1]
+        name_of_faculty = pd.Series(df['name_of_faculty'])
+        position_of_faculty = pd.Series(df['position_of_faculty'])
+        bio_of_faculty = pd.Series(df['bio_of_faculty'])
+        mail_of_faculty = pd.Series(df['mail_of_faculty'])
+        faculty_research_interest = pd.Series(df['faculty_research_interest'])
+        for i in range(nor):
+            teachers_scrapped_data = Teachers_scrapped_data(name_of_faculty=name_of_faculty[i],position_of_faculty=position_of_faculty[i],bio_of_faculty=bio_of_faculty[i],mail_of_faculty=mail_of_faculty[i],faculty_research_interest=faculty_research_interest[i])
+            teachers_scrapped_data.save()
     return render(request,"index.html")
 
 def login(request):
