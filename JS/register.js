b038388c91256bcc9f334c275ae73aaca4e1f641
Krishna Sharma <krishna16sharma.s@gmail.com>
2021-03-28 15:14:44 +0530

    Reorganized Files

--- JS/register.js
+++
@@ -1,9 +0,0 @@
-function signup_students_redirect()
-{
-window.open("../html/signup_students.html",target='_self')
-}
-
-function signup_teachers_redirect()
-{
-window.open("../html/signup_teachers.html",target='_self')
-}
