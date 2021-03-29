b8239022d8921dcf91a01df64ba72604f223327c
Aishwarya S R <sraishusr@gmail.com>
2021-03-29 07:49:55 +0530

    changed views.py after adding organized scrapped data

--- practice/profile_builder/views.py
+++ practice/profile_builder/views.py
@@ -3,23 +3,31 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import auth
 import csv
-from . models import Students,State,Teachers,Teachers_scrapped_data
+from . models import Students,State,Teachers,Teachers_data,Teachers_areas_of_interest
 import pandas as pd
 # Create your views here.
 
 def index(request):
-    if(Teachers_scrapped_data.objects.count()==0):
-        df = pd.read_csv('./scrapped_data_all.csv')
-        nor = df.shape[0]
-        noc = df.shape[1]
-        name_of_faculty = pd.Series(df['name_of_faculty'])
-        position_of_faculty = pd.Series(df['position_of_faculty'])
-        bio_of_faculty = pd.Series(df['bio_of_faculty'])
-        mail_of_faculty = pd.Series(df['mail_of_faculty'])
-        faculty_research_interest = pd.Series(df['faculty_research_interest'])
-        for i in range(nor):
-            teachers_scrapped_data = Teachers_scrapped_data(name_of_faculty=name_of_faculty[i],position_of_faculty=position_of_faculty[i],bio_of_faculty=bio_of_faculty[i],mail_of_faculty=mail_of_faculty[i],faculty_research_interest=faculty_research_interest[i])
-            teachers_scrapped_data.save()
+    if(Teachers_data.objects.count()==0):
+        df1 = pd.read_csv('./table1.csv')
+        nor1 = df1.shape[0]
+        name_of_faculty = pd.Series(df1['name_of_faculty'])
+        bio_of_faculty = pd.Series(df1['bio_of_faculty'])
+        mail_of_faculty = pd.Series(df1['mail_of_faculty'])
+        position = pd.Series(df1['position'])
+        department = pd.Series(df1['department'])
+        location = pd.Series(df1['location'])
+        for i in range(nor1):
+            teachers_data = Teachers_data(name_of_faculty=name_of_faculty[i],bio_of_faculty=bio_of_faculty[i],mail_of_faculty=mail_of_faculty[i],position=position[i],location=location[i],department=department[i])
+            teachers_data.save()
+    if(Teachers_areas_of_interest.objects.count()==0):
+        df2 = pd.read_csv('./table2.csv')
+        nor2 = df2.shape[0]
+        name_of_faculty = pd.Series(df2['name_of_faculty'])
+        faculty_research_interest = pd.Series(df2['Area of interest'])
+        for i in range(nor2):
+            teachers_areas_of_interest = Teachers_areas_of_interest(name_of_faculty=name_of_faculty[i],faculty_research_interest=faculty_research_interest[i])
+            teachers_areas_of_interest.save()       
     return render(request,"index.html")
 
 def login(request):
