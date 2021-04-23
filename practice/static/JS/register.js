4f16cf684780ae8f99bae199d18d3f32b9f9e2d7
Aishwarya S R <sraishusr@gmail.com>
2021-04-23 21:59:25 +0530

    updated after edit profile

--- practice/static/JS/register.js
+++ practice/static/JS/register.js
@@ -7,3 +7,18 @@ function signup_teachers_redirect()
 {
 window.open('signup_teachers',target='_self')
 }
+
+function changeMail_redirect()
+{
+window.open('changeMail',target='_self')
+}
+
+function changeProfilePic_redirect()
+{
+window.open('changeProfilePic',target='_self')
+}
+
+function changePassword_redirect()
+{
+window.open('changePassword',target='_self')
+}
