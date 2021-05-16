2c2fb0ee8f7d67c96e8cbd53a972b25169d51e76
Aishwarya S R <sraishusr@gmail.com>
2021-05-16 20:37:32 +0530

    updated after admin and favourites page

--- practice/static/JS/register.js
+++ practice/static/JS/register.js
@@ -22,3 +22,104 @@ function changePassword_redirect()
 {
 window.open('changePassword',target='_self')
 }
+
+function admin_areas_redirect()
+{
+window.open('adminAreas',target='_self')
+}
+
+function admin_teachers_data_redirect()
+{
+window.open('adminTeachers',target='_self')
+}
+
+function admin_user_redirect()
+{
+window.open('adminUser',target='_self')
+}
+
+function admin_student_data_redirect()
+{
+window.open('adminStudentData',target='_self')
+}
+
+function admin_teacher_data_redirect()
+{
+window.open('adminTeacherData',target='_self')
+}
+
+function areas_data_view()
+{
+window.open('areasDataView',target='_self')
+}
+
+function areas_data_insert()
+{
+window.open('areasDataInsert',target='_self')
+}
+
+function areas_data_update()
+{
+window.open('areasDataUpdate',target='_self')
+}
+
+function areas_data_delete()
+{
+window.open('areasDataDelete',target='_self')
+}
+
+function teachers_data_view()
+{
+window.open('teachersDataView',target='_self')
+}
+
+function teachers_data_insert()
+{
+window.open('teachersDataInsert',target='_self')
+}
+
+function teachers_data_update()
+{
+window.open('teachersDataUpdate',target='_self')
+}
+
+function teachers_data_delete()
+{
+window.open('teachersDataDelete',target='_self')
+}
+
+function teacher_data_view()
+{
+window.open('teacherDataView',target='_self')
+}
+
+function teacher_data_delete()
+{
+window.open('teacherDataDelete',target='_self')
+}
+
+function student_data_view()
+{
+window.open('studentDataView',target='_self')
+}
+
+function student_data_delete()
+{
+window.open('studentDataDelete',target='_self')
+}
+
+function favourites_view()
+{
+window.open('favouritesView',target='_self')
+}
+
+function favourites_insert()
+{
+window.open('favouritesInsert',target='_self')
+}
+
+function favourites_delete()
+{
+window.open('favouritesDelete',target='_self')
+}
+
